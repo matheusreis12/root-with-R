08ac15c8f717363b16d3a993fb62e90f6ae33e86
Matheus Reis <45612748+matheusreis12@users.noreply.github.com>
2019-01-16 23:17:33 -0200

    Add files via upload

---
+++ Root.R
@@ -0,0 +1,13 @@
+# Root.
+
+# Use the function sqrt(value).
+# For example.
+
+  sqrt(4)
+
+# For the other base.
+# For example 3V8(cubic root of eight).
+
+  8 ^ (1/3)
+
+# Raise to the reverse of the base.
